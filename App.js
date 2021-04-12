0fc5cce4f4ae39657af2e9902a5eac170f8e2ca4
emanu7478 <emanu7478@gmail.com>
2021-04-12 22:29:18 +0530

    add home screen skeleton andbasic screen navigation

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import AppIndex from './src';
 
 function App() {
-  return <SafeAreaProvider />;
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <AppIndex />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
 }
 export default App;
